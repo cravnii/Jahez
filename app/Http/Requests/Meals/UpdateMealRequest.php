6e68ac4c75d363d6e7b910ce3f026e8c7534f97b
Raghad <ryb_234@hotmail.com>
2023-04-18 21:53:42 +0300

    newedit  migration

---
+++ app/Http/Requests/Meals/UpdateMealRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Meals;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateMealRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+                'id' => ['nullable', 'integer'],
+                'restaurant_id'=> ['nullable', 'integer', 'exists:restaurants,id'],
+                'name'=> ['nullable', 'string'],
+                'price'=> ['nullable', 'integer'],
+            ];
+    }
+}
---
+++ app/Http/Resources/Meals/MealsResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Meals;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MealsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'restaurant_id'=> $this->restaurant_id,
+            'name'=> $this->name,
+            'price'=> $this->price,
+
+        ];
+    }
+}
